fd6f15f7e46ffd551960c782700187c027a2db9f
FeerJoaquin <andres1998joaquin@gmail.com>
2024-03-22 10:42:19 -0600

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,5 +1,5 @@
 // paste in your published Google Sheets URL from the browser address bar
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1FbCj08oXyaRHuUgaWQDYEGW9CBWfyIocfqEo-s55aZc/edit#gid=164271551';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1FbCj08oXyaRHuUgaWQDYEGW9CBWfyIocfqEo-s55aZc/edit#gid=0';
 
 // insert your own Google Sheets API key from https://console.developers.google.com
 var googleApiKey = 'AIzaSyCAJ5trI8xzfOwMGqRjc4-ewUbpsnW8rU0';
